ca0b8fbcfc4f1590c658cb45783e0c64484ca973
AleksandarPetkov <a.petkovpetkov@gmail.com>
2019-03-04 22:08:17 +0200

    Add User - Entity, Model, Repository, Service

---
+++ exodia/src/main/java/exodia/service/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package exodia.service;
+
+import exodia.domain.entities.User;
+import exodia.domain.models.service.UserServiceModel;
+import exodia.repository.UserRepository;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public boolean registerUser(UserServiceModel serviceModel) {
+        User user = this.modelMapper.map(serviceModel, User.class);
+        user.setPassword(DigestUtils.sha256Hex(user.getPassword()));
+
+        try {
+            this.userRepository.saveAndFlush(user);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public UserServiceModel loginUser(UserServiceModel serviceModel) {
+        return null;
+    }
+}
---
+++ exodia/src/main/java/exodia/web/controllers/UserController.java
@@ -0,0 +1,16 @@
+package exodia.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/register")
+    public ModelAndView modelAndView(ModelAndView modelAndView){
+        modelAndView.setViewName("register");
+        return  modelAndView;
+    }
+
+}
