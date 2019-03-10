c4b9b47faee63bdd1cc3477cab09ba167aa643df
AleksandarPetkov <a.petkovpetkov@gmail.com>
2019-03-10 10:15:32 +0200

    All Methods in SupplierService are tested

--- Unit Testing/CarDealer/src/test/java/org/softuni/cardealer/service/SupplierServiceTests.java
+++ Unit Testing/CarDealer/src/test/java/org/softuni/cardealer/service/SupplierServiceTests.java
@@ -18,6 +18,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class SupplierServiceTests {
+    private static final String INVALID_ID = "RandomID";
 
     @Autowired
     private SupplierRepository supplierRepository;
@@ -57,7 +58,7 @@ public class SupplierServiceTests {
         supplierTest.setImporter(true);
 
         //Act and Assert Exception
-        SupplierServiceModel actualSupplier = supplierService.editSupplier(supplierTest);
+        SupplierServiceModel actualSupplier = supplierService.saveSupplier(supplierTest);
 
     }
 
@@ -86,6 +87,95 @@ public class SupplierServiceTests {
         Assert.assertEquals(expectedSupplier.getName(), actualSupplier.getName());
         Assert.assertEquals(expectedSupplier.isImporter(), actualSupplier.isImporter());
 
+    }
+
+    @Test(expected = Exception.class)
+    public void editSupplier_whenHaveNull_expectException(){
+        //Arrange
+        SupplierService supplierService = new SupplierServiceImpl(supplierRepository, modelMapper);
+
+        Supplier supplierTest = new Supplier();
+        supplierTest.setName("Dori");
+        supplierTest.setImporter(true);
+        this.supplierRepository.save(supplierTest);
+
+        //Act and Assert Exception
+        SupplierServiceModel methodSupplier = new SupplierServiceModel();
+        methodSupplier.setId(supplierTest.getId());
+        methodSupplier.setName(null);
+        methodSupplier.setImporter(false);
+
+        SupplierServiceModel actualSupplier = supplierService.editSupplier(methodSupplier);
+
+    }
+
+    @Test
+    public void deleteSupplier_whenDelete_expectCorrectDelete(){
+        //Arrange
+        SupplierService supplierService = new SupplierServiceImpl(supplierRepository, modelMapper);
+
+        Supplier savedSupplier = new Supplier();
+        savedSupplier.setName("AAA");
+        savedSupplier.setImporter(true);
+
+        this.supplierRepository.save(savedSupplier);
+
+        //Act
+        SupplierServiceModel expectedSupplier =  this.modelMapper.map(this.supplierRepository.findAll().get(0), SupplierServiceModel.class);
+        SupplierServiceModel actualSupplier = supplierService.deleteSupplier(savedSupplier.getId());
+
+        //Assert
+        Assert.assertEquals(expectedSupplier.getId(), actualSupplier.getId());
+        Assert.assertEquals(expectedSupplier.getName(), actualSupplier.getName());
+        Assert.assertEquals(expectedSupplier.isImporter(), actualSupplier.isImporter());
+    }
+
+    @Test(expected = Exception.class)
+    public void deleteSupplier_whenIdIsDifferent_expectException(){
+        //Arrange
+        SupplierService supplierService = new SupplierServiceImpl(supplierRepository, modelMapper);
+
+        Supplier savedSupplier = new Supplier();
+        savedSupplier.setName("AAA");
+        savedSupplier.setImporter(true);
+        this.supplierRepository.save(savedSupplier);
+
+        //Act
+        supplierService.deleteSupplier(INVALID_ID);
+    }
+
+    @Test
+    public void findSupplierById_whenCorrectId_expectCorrectSupplier(){
+        //Arrange
+        SupplierService supplierService = new SupplierServiceImpl(supplierRepository, modelMapper);
+
+        Supplier savedSupplier = new Supplier();
+        savedSupplier.setName("AAA");
+        savedSupplier.setImporter(true);
+        this.supplierRepository.save(savedSupplier);
+
+        //Act
+        SupplierServiceModel actualSupplier = supplierService.findSupplierById(savedSupplier.getId());
+        SupplierServiceModel expectedSupplier =  this.modelMapper.map(this.supplierRepository.findAll().get(0), SupplierServiceModel.class);
+
+        //Assert
+        Assert.assertEquals(expectedSupplier.getId(), actualSupplier.getId());
+        Assert.assertEquals(expectedSupplier.getName(), actualSupplier.getName());
+        Assert.assertEquals(expectedSupplier.isImporter(), actualSupplier.isImporter());
+    }
+
+    @Test(expected = Exception.class)
+    public void findSupplierById_whenIdIsDifferent_expectException(){
+        //Arrange
+        SupplierService supplierService = new SupplierServiceImpl(supplierRepository, modelMapper);
+
+        Supplier savedSupplier = new Supplier();
+        savedSupplier.setName("AAA");
+        savedSupplier.setImporter(true);
+        this.supplierRepository.save(savedSupplier);
+
+        //Act
+        supplierService.findSupplierById(INVALID_ID);
     }
 
 }
